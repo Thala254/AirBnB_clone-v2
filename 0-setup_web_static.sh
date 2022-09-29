40452a7a1310c043bdcf8ac5d260b0c504ebe1f6
17Keith <kayomega7@gmail.com>
2022-09-29 16:59:17 +0300

    Static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" >/data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
